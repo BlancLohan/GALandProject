62d227b6b45d6dc0e853eb950a876e9feb0b0ca5
RhadamanthysGH <45771234+RhadamanthysGH@users.noreply.github.com>
2021-04-06 18:17:45 +0200

    Base Project + Replication Bullet

---
+++ Source/ProjectFPSTest/FPSPlayerSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "FPSPlayerSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTFPSTEST_API UFPSPlayerSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
